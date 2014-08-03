634d72c23d81ca259018999a8d818858d58943e9
maiatoday <maiatoday@gmail.com>
2014-08-03 22:54:22 +0200

    Android HelloWorld with App-engine

---
+++ app/src/main/java/za/co/maiatoday/helloappengine/MyActivity.java
@@ -0,0 +1,51 @@
+package za.co.maiatoday.helloappengine;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MyActivity extends Activity {
+
+    private EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new EndpointsAsyncTask().execute(new Pair<Context, String>(MyActivity.this, et.getText().toString()));
+            }
+        });
+        et = (EditText) findViewById(R.id.editText);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
